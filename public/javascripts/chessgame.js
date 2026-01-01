614dddc53f4740829dd73da1ebd081b15b7e54bf
Aditya Thakur <adityathakur6076@gmail.com>
2026-01-01 21:59:52 +0530

    chess board angle view updated

--- public/javascripts/chessgame.js
+++ public/javascripts/chessgame.js
@@ -78,6 +78,12 @@ const renderBoard = () => {
             boardElement.appendChild(squareElement)
         })
     })
+    if(playerRole=='b'){
+        boardElement.classList.add('flipped')
+    }
+    else{
+        boardElement.classList.remove('flipped')
+    }
 }
 
 
