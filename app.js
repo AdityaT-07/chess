17660478e4a60c6935cc5e63c9cad231647287f1
Aditya Thakur <adityathakur6076@gmail.com>
2026-01-01 21:51:37 +0530

    pieces move implmented

--- app.js
+++ app.js
@@ -11,57 +11,52 @@ const io = socket(server)
 const chess = new Chess()
 
 const player ={};
-const currentPlayer = 'w';
+let currentPlayer = 'w';
 
 app.use(express.static(path.join(__dirname,'public')))
 app.set('view engine','ejs');
  
-io.on('connection',(uniquesocket)=>{
-    console.log("connection establish");
-        if(!player.white){
-            player.white =uniquesocket.id
-            uniquesocket.emit('playerRole','w')
-        }
-        else if(!player.black){
-            player.black = uniquesocket.id
-            uniquesocket.emit('playerRole','b')
-        }
-        else{
-            uniquesocket.emit('playerRole')
-        }
+io.on('connection', (uniquesocket) => {
 
-        uniquesocket.on('move',(move)=>{
-          try{
-              if(chess.turn()==='w' && uniquesocket.id!==player.white) return;
-            if(chess.turn()==='b' && uniquesocket.id!==player.black) return;
+    if (!player.white) {
+        player.white = uniquesocket.id
+        uniquesocket.emit('playerRole', 'w')
+    }
+    else if (!player.black) {
+        player.black = uniquesocket.id
+        uniquesocket.emit('playerRole', 'b')
+    }
+    else {
+        uniquesocket.emit('spectatorRole') // correction
+    }
 
-            const result =chess.move(move);
-            if(result){
-                currentPlayer = chess.turn()
-                io.emit('move',move)
-                io.emit('currentState',chess.fen())
+    uniquesocket.on('move', (move) => {
+        try {
+            if (chess.turn() === 'w' && uniquesocket.id !== player.white) return
+            if (chess.turn() === 'b' && uniquesocket.id !== player.black) return
+
+            const result = chess.move(move)
+
+            if (!result) {
+                uniquesocket.emit('invalidMove', move)
+                return
             }
-            else{
-                console.log("invalid move : ",move);
-                uniquesocket.emit('invalidMove',move)
-                
-            }
-          }
-          catch(err){
-            console.log("invalid move : ",move);
-            console.log(err);
-            
-          }
-        })
 
-    uniquesocket.on('disconnect',()=>{
-        if(uniquesocket.id ===player.white){
-            delete player.white
-        }else if(uniquesocket.id ===player.black){
-            delete player.black
+            currentPlayer = chess.turn()
+            io.emit('move', move)
+            io.emit('boardState', chess.fen()) //correction
+        }
+        catch (err) {
+            console.error(err)
         }
     })
+
+    uniquesocket.on('disconnect', () => {
+        if (uniquesocket.id === player.white) delete player.white
+        if (uniquesocket.id === player.black) delete player.black
+    })
 })
+
 
 app.get('/',(req,res)=>{
     res.render('index')
--- public/javascripts/chessgame.js
+++ public/javascripts/chessgame.js
@@ -81,8 +81,14 @@ const renderBoard = () => {
 }
 
 
-const handleMove =  ()=>{
+const handleMove =  (source, target)=>{
+ const move ={
+    from : `${String.fromCharCode(97 +source.col)}${8-source.row}` ,
+    to :`${String.fromCharCode(97 +target.col)}${8-target.row}` ,
+    promotion : 'q'
+ }
 
+ socket.emit('move',move)
 }
 
 const getPieceUnicode = (piece)=>{
@@ -108,4 +114,23 @@ return unicodePieces[piece.type] || ''
 
 }
 
+socket.on('playerRole',(role)=>{
+    playerRole = role
+    renderBoard()
+})
+
+socket.on('spectatorRole',()=>{
+    playerRole = null;
+    renderBoard()
+})
+
+socket.on('boardState',(fen)=>{
+    chess.load(fen)
+    renderBoard()
+})
+
+socket.on('move',(move)=>{
+    chess.move(move)
+    renderBoard()
+})
 renderBoard()
