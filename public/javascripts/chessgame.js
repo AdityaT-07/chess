8de8e4ab5ed9482c297ca29898b9e65f2a38d0f3
Aditya Thakur <adityathakur6076@gmail.com>
2026-01-01 20:37:36 +0530

    blank board render & styling implemented

--- public/javascripts/chessgame.js
+++ public/javascripts/chessgame.js
@@ -1,1 +1,50 @@
+// const { Chess } = require("chess.js")
+
 const socket = io()
+const chess = new Chess()
+const boardElement = document.getElementsByClassName('chessboard')
+
+let draggedPiece = null
+let sourceSquare = null
+let playerRole = null
+
+const renderBoard = ()=>{
+ const board = chess.board()
+ boardElement.innerHTML = ''
+ board.forEach((row,rowIndex)=>{
+    row.forEach((square,squareIndex)=>{
+        const squareElement = document.createElement('div')
+        squareElement.classList.add('square',
+            (rowIndex + squareIndex)%2===0?'light' :'dark'
+        )
+        squareElement.dataset.row =rowIndex
+        squareElement.dataset.col = squareIndex
+
+        if(square){
+            let pieceElement = document.createElement('div')
+            pieceElement.classList.add('piece',
+                square.color==='w'?'white':'black'
+            )
+            pieceElement.innerText= ''
+            pieceElement.draggable = playerRole === square.color
+
+            pieceElement.addEventListener('dragstart',()=>{
+                if(pieceElement.draggable){
+                    draggedPiece = pieceElement  // draggedPiece set
+                    sourceSquare = {row : rowIndex , col : squareIndex} 
+                    // sourceSquare set
+                    
+                }
+            })
+
+        }
+    })
+ })
+}
+const handleMove =  ()=>{
+
+}
+
+const getPieceUnicode = ()=>{
+
+}
